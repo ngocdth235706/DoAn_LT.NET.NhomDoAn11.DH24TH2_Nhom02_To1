aae39bf36cc96ceac38eacbfceb2a93cc837fa5d
ngocdth235706 <ngoc_dth235706@student.agu.edu.vn>
2025-11-16 17:26:33 +0700

    Add project files.

---
+++ Do_an_NET/Do_an_NET/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Do_an_NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvDanhSachXe = new DataGridView();
+            txtTimKiem = new TextBox();
+            btnTim = new Button();
+            btnTaiLai = new Button();
+            txtMaXe = new TextBox();
+            txtTenXe = new TextBox();
+            txtHangXe = new TextBox();
+            txtMauXe = new TextBox();
+            txtGiaXe = new TextBox();
+            txtSoLuong = new TextBox();
+            btnThem = new Button();
+            btnLuu = new Button();
+            btnCapNhat = new Button();
+            btnXoa = new Button();
+            btnHuy = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            labelTimKiem = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvDanhSachXe).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvDanhSachXe
+            // 
+            dgvDanhSachXe.BackgroundColor = SystemColors.ButtonHighlight;
+            dgvDanhSachXe.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDanhSachXe.Location = new Point(12, 103);
+            dgvDanhSachXe.Name = "dgvDanhSachXe";
+            dgvDanhSachXe.RowHeadersWidth = 62;
+            dgvDanhSachXe.Size = new Size(1044, 232);
+            dgvDanhSachXe.TabIndex = 0;
+            dgvDanhSachXe.CellClick += dgvDanhSachXe_CellClick;
+            // 
+            // txtTimKiem
+            // 
+            txtTimKiem.Location = new Point(106, 64);
+            txtTimKiem.Name = "txtTimKiem";
+            txtTimKiem.Size = new Size(674, 31);
+            txtTimKiem.TabIndex = 1;
+            // 
+            // btnTim
+            // 
+            btnTim.Location = new Point(786, 65);
+            btnTim.Name = "btnTim";
+            btnTim.Size = new Size(85, 32);
+            btnTim.TabIndex = 2;
+            btnTim.Text = "Tìm";
+            btnTim.UseVisualStyleBackColor = true;
+            btnTim.Click += btnTim_Click;
+            // 
+            // btnTaiLai
+            // 
+            btnTaiLai.Location = new Point(877, 65);
+            btnTaiLai.Name = "btnTaiLai";
+            btnTaiLai.Size = new Size(85, 32);
+            btnTaiLai.TabIndex = 3;
+            btnTaiLai.Text = "Tải Lại";
+            btnTaiLai.UseVisualStyleBackColor = true;
+            btnTaiLai.Click += btnTaiLai_Click;
+            // 
+            // txtMaXe
+            // 
+            txtMaXe.Location = new Point(106, 341);
+            txtMaXe.Name = "txtMaXe";
+            txtMaXe.Size = new Size(213, 31);
+            txtMaXe.TabIndex = 4;
+            // 
+            // txtTenXe
+            // 
+            txtTenXe.Location = new Point(441, 341);
+            txtTenXe.Name = "txtTenXe";
+            txtTenXe.Size = new Size(213, 31);
+            txtTenXe.TabIndex = 5;
+            // 
+            // txtHangXe
+            // 
+            txtHangXe.Location = new Point(758, 341);
+            txtHangXe.Name = "txtHangXe";
+            txtHangXe.Size = new Size(213, 31);
+            txtHangXe.TabIndex = 6;
+            // 
+            // txtMauXe
+            // 
+            txtMauXe.Location = new Point(106, 390);
+            txtMauXe.Name = "txtMauXe";
+            txtMauXe.Size = new Size(213, 31);
+            txtMauXe.TabIndex = 7;
+            // 
+            // txtGiaXe
+            // 
+            txtGiaXe.Location = new Point(441, 390);
+            txtGiaXe.Name = "txtGiaXe";
+            txtGiaXe.Size = new Size(213, 31);
+            txtGiaXe.TabIndex = 8;
+            // 
+            // txtSoLuong
+            // 
+            txtSoLuong.Location = new Point(758, 390);
+            txtSoLuong.Name = "txtSoLuong";
+            txtSoLuong.Size = new Size(213, 31);
+            txtSoLuong.TabIndex = 9;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(62, 480);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(171, 43);
+            btnThem.TabIndex = 10;
+            btnThem.Text = "Thêm";
+            btnThem.UseVisualStyleBackColor = true;
+            btnThem.Click += btnThem_Click;
+            // 
+            // btnLuu
+            // 
+            btnLuu.Location = new Point(252, 480);
+            btnLuu.Name = "btnLuu";
+            btnLuu.Size = new Size(171, 43);
+            btnLuu.TabIndex = 11;
+            btnLuu.Text = "Lưu";
+            btnLuu.UseVisualStyleBackColor = true;
+            btnLuu.Click += btnLuu_Click;
+            // 
+            // btnCapNhat
+            // 
+            btnCapNhat.Location = new Point(441, 480);
+            btnCapNhat.Name = "btnCapNhat";
+            btnCapNhat.Size = new Size(171, 43);
+            btnCapNhat.TabIndex = 12;
+            btnCapNhat.Text = "Cập nhật";
+            btnCapNhat.UseVisualStyleBackColor = true;
+            btnCapNhat.Click += btnCapNhat_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(630, 480);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(171, 43);
+            btnXoa.TabIndex = 13;
+            btnXoa.Text = "Xóa";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // btnHuy
+            // 
+            btnHuy.Location = new Point(820, 480);
+            btnHuy.Name = "btnHuy";
+            btnHuy.Size = new Size(171, 43);
+            btnHuy.TabIndex = 14;
+            btnHuy.Text = "Hủy";
+            btnHuy.UseVisualStyleBackColor = true;
+            btnHuy.Click += btnHuy_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.RoyalBlue;
+            label1.Location = new Point(394, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(284, 45);
+            label1.TabIndex = 15;
+            label1.Text = "QUẢN LÝ XE MÁY";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 347);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 25);
+            label2.TabIndex = 16;
+            label2.Text = "Mã xe:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(26, 396);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 25);
+            label3.TabIndex = 17;
+            label3.Text = "Màu xe: ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(364, 344);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 25);
+            label4.TabIndex = 18;
+            label4.Text = "Tên xe:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(364, 390);
+            label5.Name = "label5";
+            label5.Size = new Size(63, 25);
+            label5.TabIndex = 19;
+            label5.Text = "Giá xe:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(660, 347);
+            label6.Name = "label6";
+            label6.Size = new Size(81, 25);
+            label6.TabIndex = 20;
+            label6.Text = "Hãng xe:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(660, 396);
+            label7.Name = "label7";
+            label7.Size = new Size(89, 25);
+            label7.TabIndex = 21;
+            label7.Text = "Số lượng:";
+            // 
+            // labelTimKiem
+            // 
+            labelTimKiem.AutoSize = true;
+            labelTimKiem.Location = new Point(12, 67); // Vị trí gần txtTimKiem
+            labelTimKiem.Name = "labelTimKiem";
+            labelTimKiem.Size = new Size(88, 25);
+            labelTimKiem.TabIndex = 22; // Thay đổi số index nếu cần
+            labelTimKiem.Text = "Tìm kiếm:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PowderBlue;
+            ClientSize = new Size(1068, 547);
+            Controls.Add(labelTimKiem);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnHuy);
+            Controls.Add(btnXoa);
+            Controls.Add(btnCapNhat);
+            Controls.Add(btnLuu);
+            Controls.Add(btnThem);
+            Controls.Add(txtSoLuong);
+            Controls.Add(txtGiaXe);
+            Controls.Add(txtMauXe);
+            Controls.Add(txtHangXe);
+            Controls.Add(txtTenXe);
+            Controls.Add(txtMaXe);
+            Controls.Add(btnTaiLai);
+            Controls.Add(btnTim);
+            Controls.Add(txtTimKiem);
+            Controls.Add(dgvDanhSachXe);
+            Name = "Form1";
+            Text = "Quản Lý Xe Máy";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvDanhSachXe).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dgvDanhSachXe;
+        private TextBox txtTimKiem;
+        private Button btnTim;
+        private Button btnTaiLai;
+        private TextBox txtMaXe;
+        private TextBox txtTenXe;
+        private TextBox txtHangXe;
+        private TextBox txtMauXe;
+        private TextBox txtGiaXe;
+        private TextBox txtSoLuong;
+        private Button btnThem;
+        private Button btnLuu;
+        private Button btnCapNhat;
+        private Button btnXoa;
+        private Button btnHuy;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label labelTimKiem;
+    }
+}
---
+++ Do_an_NET/Do_an_NET/Form1.cs
@@ -0,0 +1,278 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Do_an_NET
+{
+    public partial class Form1 : Form
+    {
+        // Biến trạng thái để xác định đang Thêm mới hay Cập nhật
+        private bool isAddingNew = false;
+
+        // Chuỗi kết nối cố định
+        private readonly string connectionString = "Server=localhost;Database=qlcuahangxemay;Uid=root;Pwd=DoAn_Python_DH24TH2;";
+        public Form1()
+        {
+            InitializeComponent();
+            // Thiết lập chế độ ban đầu: chỉ xem, khóa các ô nhập liệu
+            
+        }
+
+        // ĐẶT PHƯƠNG THỨC Ở ĐÂY (BÊN TRONG CLASS Form1)
+        public void LoadData(string keyword = "")
+        {
+            string sqlQuery = "SELECT MaXe, TenXe, HangXe, MauXe, GiaXe, SoLuong, CreatedAt FROM xemay";
+            // Cập nhật Chuỗi Kết Nối CSDL của bạn tại đây!
+            string connectionString = "Server=localhost;Database=qlcuahangxemay;Uid=root;Pwd=DoAn_Python_DH24TH2;";
+
+            // Thêm điều kiện tìm kiếm nếu có từ khóa
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                // Tìm kiếm LIKE trong nhiều cột (TenXe, MaXe, HangXe)
+                sqlQuery += " WHERE TenXe LIKE @Keyword OR MaXe LIKE @Keyword OR HangXe LIKE @Keyword";
+            }
+            // TRUY VẤN LẤY TẤT CẢ DỮ LIỆU ĐỂ HIỂN THỊ
+            // Bỏ mệnh đề WHERE và tham số vì chúng ta lấy toàn bộ dữ liệu
+          
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // 1. Khởi tạo Adapter và DataTable
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(sqlQuery, conn);
+                    DataTable dataTable = new DataTable();
+
+                    // Thêm tham số tìm kiếm an toàn (chỉ khi có từ khóa)
+                    if (!string.IsNullOrEmpty(keyword))
+                    {
+                        adapter.SelectCommand.Parameters.AddWithValue("@Keyword", $"%{keyword}%");
+                    }
+                    // 2. Lấp đầy (Fill) DataTable bằng dữ liệu từ CSDL
+                    adapter.Fill(dataTable);
+
+                    // 3. Gán DataTable làm nguồn dữ liệu cho DataGridView
+                    dgvDanhSachXe.DataSource = dataTable;
+
+                    // Tự động điều chỉnh độ rộng cột cho phù hợp
+                    dgvDanhSachXe.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+                }
+                catch (MySqlException ex)
+                {
+                    // Hiển thị lỗi ra thông báo hộp thoại
+                    MessageBox.Show("LỖI TẢI DỮ LIỆU CSDL: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void ClearTextBoxes()
+        {
+            txtMaXe.Text = "";
+            txtTenXe.Text = "";
+            txtHangXe.Text = "";
+            txtMauXe.Text = "";
+            txtGiaXe.Text = "";
+            txtSoLuong.Text = "";
+        }
+        private void SetEditMode(bool isEnabled)
+        {
+            // Bật/tắt các ô nhập liệu
+            txtTenXe.ReadOnly = !isEnabled;
+            txtHangXe.ReadOnly = !isEnabled;
+            txtMauXe.ReadOnly = !isEnabled;
+            txtGiaXe.ReadOnly = !isEnabled;
+            txtSoLuong.ReadOnly = !isEnabled;
+
+            // Mã xe chỉ được chỉnh sửa khi đang Thêm mới
+            txtMaXe.ReadOnly = !(isAddingNew && isEnabled);
+
+            // Bật/tắt các nút chức năng
+            btnLuu.Enabled = isEnabled;
+            btnHuy.Enabled = isEnabled;
+
+            btnThem.Enabled = !isEnabled;
+            btnCapNhat.Enabled = !isEnabled && !string.IsNullOrEmpty(txtMaXe.Text); // Chỉ bật Cập nhật/Xóa khi có dữ liệu được chọn
+            btnXoa.Enabled = !isEnabled && !string.IsNullOrEmpty(txtMaXe.Text);
+        }
+        // Hàm chung thực thi INSERT, UPDATE, DELETE
+        private void ExecuteNonQuery(string sqlQuery, string successMessage)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(sqlQuery, conn))
+                    {
+                        // Thêm tham số từ TextBoxes
+                        cmd.Parameters.AddWithValue("@MaXe", txtMaXe.Text);
+                        cmd.Parameters.AddWithValue("@TenXe", txtTenXe.Text);
+                        cmd.Parameters.AddWithValue("@HangXe", txtHangXe.Text);
+                        cmd.Parameters.AddWithValue("@MauXe", txtMauXe.Text);
+
+                        // Chuyển đổi sang kiểu số, dùng TryParse để kiểm tra lỗi nhập liệu
+                        if (!decimal.TryParse(txtGiaXe.Text, out decimal giaXe))
+                            throw new Exception("Giá xe không hợp lệ.");
+                        if (!int.TryParse(txtSoLuong.Text, out int soLuong))
+                            throw new Exception("Số lượng không hợp lệ.");
+
+                        cmd.Parameters.AddWithValue("@GiaXe", giaXe);
+                        cmd.Parameters.AddWithValue("@SoLuong", soLuong);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show(successMessage, "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            LoadData(); // Tải lại dữ liệu
+                            ClearTextBoxes();
+                            isAddingNew = false;
+                            SetEditMode(false);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không có bản ghi nào bị ảnh hưởng.", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("LỖI THAO TÁC CSDL: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        // CÁC SỰ KIỆN CỦA FORM VÀ CONTROLS
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Gọi hàm để tải dữ liệu ngay khi Form được mở
+            LoadData();
+        }
+        private void dgvDanhSachXe_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Kiểm tra xem có phải là hàng header không
+            if (e.RowIndex >= 0)
+            {
+                // Lấy hàng hiện tại mà người dùng đã click
+                DataGridViewRow row = dgvDanhSachXe.Rows[e.RowIndex];
+
+                // Đổ dữ liệu từ hàng đó vào các TextBox
+                txtMaXe.Text = row.Cells["MaXe"].Value.ToString();
+                txtTenXe.Text = row.Cells["TenXe"].Value.ToString();
+                txtHangXe.Text = row.Cells["HangXe"].Value.ToString();
+                txtMauXe.Text = row.Cells["MauXe"].Value.ToString();
+
+                // Sử dụng Convert.ToDecimal/ToString() để đảm bảo định dạng số không bị lỗi
+                txtGiaXe.Text = row.Cells["GiaXe"].Value.ToString();
+                txtSoLuong.Text = row.Cells["SoLuong"].Value.ToString();
+
+                // Đảm bảo các nút Cập nhật/Xóa được bật
+                SetEditMode(false);
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            isAddingNew = true;
+            ClearTextBoxes();
+            SetEditMode(true);
+            txtMaXe.Focus();
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtMaXe.Text) || string.IsNullOrEmpty(txtTenXe.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ Mã Xe và Tên Xe.", "Lỗi nhập liệu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string sql;
+            string message;
+
+            if (isAddingNew) // INSERT (Thêm mới)
+            {
+                sql = "INSERT INTO xemay (MaXe, TenXe, HangXe, MauXe, GiaXe, SoLuong, CreatedAt) " +
+                      "VALUES (@MaXe, @TenXe, @HangXe, @MauXe, @GiaXe, @SoLuong, NOW())";
+                message = "Thêm mới xe máy thành công!";
+            }
+            else // UPDATE (Cập nhật)
+            {
+                sql = "UPDATE xemay SET TenXe = @TenXe, HangXe = @HangXe, MauXe = @MauXe, " +
+                      "GiaXe = @GiaXe, SoLuong = @SoLuong WHERE MaXe = @MaXe";
+                message = "Cập nhật xe máy thành công!";
+            }
+
+            ExecuteNonQuery(sql, message);
+        }
+
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtMaXe.Text))
+            {
+                MessageBox.Show("Vui lòng chọn xe cần Cập nhật.", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            isAddingNew = false; // Chế độ Cập nhật
+            SetEditMode(true);
+            txtMaXe.ReadOnly = true; // Không cho phép sửa Mã Xe khi Cập nhật
+            txtTenXe.Focus();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtMaXe.Text))
+            {
+                MessageBox.Show("Vui lòng chọn xe cần xóa.", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (MessageBox.Show($"Bạn có chắc chắn muốn xóa xe có Mã là {txtMaXe.Text}?", "Xác nhận Xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                string sql = "DELETE FROM xemay WHERE MaXe = @MaXe";
+                ExecuteNonQuery(sql, "Xóa xe máy thành công!");
+            }
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            // 1. Xóa nội dung trong tất cả các TextBox
+            ClearTextBoxes();
+
+            // 2. Đặt lại trạng thái về chế độ xem (không Thêm mới/Cập nhật)
+            isAddingNew = false;
+            SetEditMode(false);
+
+            // 3. Tải lại toàn bộ dữ liệu (không có từ khóa tìm kiếm)
+            // SỬA: Dùng hàm LoadData() mới thay vì LoadDataFromxemay() cũ
+            LoadData();
+        }
+
+        // Hàm cho nút Tải lại (Nếu bạn đã đặt tên là btnTaiLai)
+        private void btnTaiLai_Click(object sender, EventArgs e)
+        {
+            txtTimKiem.Text = ""; // Xóa nội dung tìm kiếm
+            LoadData();
+            ClearTextBoxes();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            string keyword = txtTimKiem.Text.Trim();
+            if (string.IsNullOrEmpty(keyword))
+            {
+                LoadData(); // Nếu ô tìm kiếm trống, tải lại toàn bộ
+            }
+            else
+            {
+                LoadData(keyword); // Tìm kiếm theo từ khóa
+            }
+        }
+    }
+}
+        
